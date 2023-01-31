e6195f3737a62dd0dec5b0b2cfbaa47d7b5cd296
Kratik <gargkratik707@gmail.com>
2023-01-31 16:56:48 +0530

    first commit

---
+++ sum_of_n_even_numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std; 
+
+int main() 
+{ 
+    int n;
+    cin >> n;
+    int i = 2,sum = 0;
+    while (i<=n)
+    {
+        sum+=i;
+        i+=2;
+    }
+    cout << sum; 
+    return 0;
+}
